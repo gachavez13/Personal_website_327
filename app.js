f6b4bb2a75e7a660c8ea074d0ce185a505f371e4
gachavez13 <102621665+gachavez13@users.noreply.github.com>
2023-09-29 14:54:10 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function doStuff() {
+  var image = document.getElementById('hiddenImage');
+  if (image.style.display === 'none') {
+      image.style.display = 'block';
+  } else {
+      image.style.display = 'none';
+  }
+}
